ca9241afea52f296d4ded52df0d6733dd68e3349
joates <77863872+joates-spring-2021@users.noreply.github.com>
2022-04-12 18:29:24 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 12 18:28:16 2022
+
+@author: johnw
+"""
+
+
+# -*- coding: utf-8 -*-
+"""
+
+johnwoates
+CPSC 223P-01
+Thu Apr 12, 2022
+joates@fullerton.edu
+
+"""
